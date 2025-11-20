c7dc9d23f527b965a12f14d671a7db447f18ca13
luisfonseca664 <i49483190@gmail.com>
2025-11-19 19:49:17 -0600

    Conexion a mysql

---
+++ Backend/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Backend
+{
+    public class Conexion
+    {
+        private static string cadena = "server=localhost;database=ventas;user=root;password=1111;";
+        public static MySqlConnection ObtenerConexion()
+        {
+            return new MySqlConnection(cadena);
+        }
+    }
+}
